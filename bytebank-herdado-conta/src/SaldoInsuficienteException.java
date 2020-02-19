6ab78fddccd9a34236feadf3de4abb445c91e781
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-02-19 01:54:48 -0300

    Curso 2 Java - Orientãção a objeto

---
+++ bytebank-herdado-conta/src/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+
+public class SaldoInsuficienteException extends Exception{ //checked
+	
+	public SaldoInsuficienteException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ bytebank-herdado-conta/src/TesteContas.java
@@ -0,0 +1,22 @@
+
+public class TesteContas {
+	
+	public static void main(String[] args) throws SaldoInsuficienteException{
+		
+			
+		ContaCorrente cc = new ContaCorrente(111, 00001);
+		cc.deposita(100.0);
+		
+		ContaPoupanca cp = new ContaPoupanca(222, 00002);
+		cp.deposita(200.0);
+		
+		cc.transfere(90.0, cp);
+		
+		System.out.println("******** Saldo Final após transferência ********");
+		System.out.println("Saldo CC: " + cc.getSaldo());
+		System.out.println("Saldo CP: " + cp.getSaldo());
+		
+		
+		
+	}
+}
