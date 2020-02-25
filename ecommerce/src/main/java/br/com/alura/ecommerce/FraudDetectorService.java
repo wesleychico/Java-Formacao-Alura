b176bb9c205e3ba2173a69fd9e28ab3edb30409c
wesley francisco de castro rocha <wesleyrocha.ti@gmail.com>
2020-02-25 16:57:48 -0300

    2.3 - Paralelizando e a importância das Keys

--- ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class FraudDetectorService {
 
@@ -42,6 +43,7 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName()+ "_" +UUID.randomUUID().toString());
         return properties;
     }
 }
--- ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,6 +1,7 @@
 package br.com.alura.ecommerce;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.producer.Callback;
@@ -14,22 +15,28 @@ public class NewOrderMain {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		
 		var producer = new KafkaProducer<String, String>(properties());
-		var value = "132123,67523,7894589745";
-		var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
 		
-//		Produzir um novo pedido 
-		Callback callback = (data, ex) -> {	
-			if(ex != null) {
-				ex.printStackTrace();
-				return;
-			}
-			System.out.println("Sucesso enviando " + data.topic() + ":::partition: " + data.partition() + "/ offset: " + data.offset() + "/ timestamp: " + data.timestamp());
-		};
-		var email = "Thank you for your order! We are processing your order!";
-		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-		producer.send(record, callback).get();
-		producer.send(emailRecord, callback).get();
+		for (var i = 0; i < 100; i++) {
+						
+			var key = UUID.randomUUID().toString();
+			var value = key + ",67523,7894589745";
+			var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+			
+	//		Produzir um novo pedido 
+			Callback callback = (data, ex) -> {	
+				if(ex != null) {
+					ex.printStackTrace();
+					return;
+				}
+				System.out.println("Sucesso enviando " + data.topic() + ":::partition: " + data.partition() + "/ offset: " + data.offset() + "/ timestamp: " + data.timestamp());
+			};
+			var email = "Thank you for your order! We are processing your order!";
+			var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+			producer.send(record, callback).get();
+			producer.send(emailRecord, callback).get();
+		}
 	}
+		
 
 //	Configuracoes de onde esta rodando o kafka (IP)
 //  Transformar a mensagem de srting para bytes (Key e Valor)	
