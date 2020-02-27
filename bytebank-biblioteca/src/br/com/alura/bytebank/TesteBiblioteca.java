311bc15d95ecef0384769986442d54d6028f54fe
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-02-26 23:39:03 -0300

    3.2 - Distribuição do seu código - JAR

---
+++ bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -0,0 +1,14 @@
+package br.com.alura.bytebank;
+
+import br.com.bytebank.banco.model.Conta;
+import br.com.bytebank.banco.model.ContaCorrente;
+
+public class TesteBiblioteca {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new ContaCorrente(123, 321);
+
+	}
+
+}
