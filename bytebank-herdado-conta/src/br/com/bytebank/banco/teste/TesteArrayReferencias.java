23055b4719f7627121d0431bed246d3b51a54b0d
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-04-03 15:06:27 -0300

    Java 6 - Aula 2 - Tipo de Array

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,27 +1,29 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.model.Conta;
 import br.com.bytebank.banco.model.ContaCorrente;
+import br.com.bytebank.banco.model.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
     public static void main(String[] args) {
 
-        ContaCorrente[] arrayContas = new ContaCorrente[5];
+        Conta[] contas = new ContaCorrente[5];
 
         ContaCorrente conta1 = new ContaCorrente(22,11);
-        ContaCorrente conta2 = new ContaCorrente(22,22);
+        contas[0] = conta1;
 
-        arrayContas[0] = conta1;
-        arrayContas[1] = conta2;
+        ContaPoupanca conta2 = new ContaPoupanca(22, 22);
+        contas[1] = conta2;
 
-        System.out.println("Primeira posicoes do Array = " + arrayContas[0]);
-        System.out.println("Numero da primeira conta = " + arrayContas[0].getNumero());
+
+        System.out.println("Numero da primeira conta = " + contas[1].getNumero());
 
         // Forma literal de criar Array
 
-        int[] contas = {22,33,44};
-
-        System.out.println("Array forma literal = " + contas[1]);
+        ContaCorrente ref = (ContaCorrente) contas[0];
+        System.out.println(conta2.getNumero());
+        System.out.println(ref.getNumero());
 
     }
 }
