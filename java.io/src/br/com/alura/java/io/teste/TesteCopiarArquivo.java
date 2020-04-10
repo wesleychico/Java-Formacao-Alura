7c40352b4d9bb85849778f2c818dc93e89bd950a
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-04-10 10:30:09 -0300

    Aula 2- Escrita com java.io (Java Parte 7 - Trabalhando com java.io

--- java.io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ java.io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -1,4 +1,38 @@
 package br.com.alura.java.io.teste;
 
+import java.io.*;
+
 public class TesteCopiarArquivo {
+    public static void main(String[] args) throws IOException {
+
+        //InputStream fis = new FileInputStream("lorem"); //Leitura de um arquivo
+        InputStream fis = System.in; // Leitura no console
+        Reader isr = new InputStreamReader(fis); // Leitura do console
+        BufferedReader br = new BufferedReader(isr);
+
+        // OutputStream fos = new FileOutputStream("Lorem-saida"); // Gerando saida no arquivo
+
+        OutputStream fos = System.out; // Gerando saida no console
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        OutputStream fos2 = new FileOutputStream("Lorem-saida"); //Gerar saida no arquivo tambem
+        Writer osw2 = new OutputStreamWriter(fos2); // Transformando caracteres em byte
+        BufferedWriter bw2 = new BufferedWriter(osw2);
+
+        String linha = br.readLine();
+
+        while (linha != null && !linha.isEmpty()){
+            bw.write(linha);
+            bw.newLine();
+            bw.flush();
+            bw2.write(linha);
+            bw2.newLine();
+            bw2.flush();
+            linha = br.readLine();
+        }
+        br.close();
+        bw.close();
+        bw2.close();
+    }
 }
--- java.io/src/br/com/alura/java/io/teste/TesteEscrita.java
+++ java.io/src/br/com/alura/java/io/teste/TesteEscrita.java
@@ -1,4 +1,18 @@
 package br.com.alura.java.io.teste;
 
+import java.io.*;
+
 public class TesteEscrita {
+    public static void main(String[] args) throws IOException {
+
+        OutputStream fos = new FileOutputStream("Lorem-saida"); //Gerar Arquivo
+        Writer osw = new OutputStreamWriter(fos); // Transformar caracteres em byte
+        BufferedWriter  bw = new BufferedWriter(osw); //Gravar Linha
+
+        bw.write("Teste escrita no arquivo de saida");
+        bw.newLine();
+        bw.write("Teste Linha 2");
+        bw.close();
+
+    }
 }
