f51d9b7c8f3833e01a60aff05b6ecb98e025dca6
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-04-10 09:23:21 -0300

    Aula 1: Leitura com Java.io (Java parte 7: Trabalhando com java.io)

---
+++ java.io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,24 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteLeitura {
+    public static void main(String[] args) throws IOException {
+
+        //InputStreamReader isr = new InputStreamReader(fis);
+        //FileInputStream fis = new FileInputStream("lorem.txt");
+
+        //Forma generica
+        InputStream fis = new FileInputStream("lorem"); // Transformar Bytes em Caracteres
+        Reader isr = new InputStreamReader(fis); // Transformar Bytes em Caracteres
+        BufferedReader br = new BufferedReader(isr); // Ler a linha toda
+
+        String linha = br.readLine();
+
+        while (linha != null){
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+        br.close();
+    }
+}
