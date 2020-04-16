f02fd9a32c713ced235f4c6da57319a32f03b4b2
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-04-16 13:49:59 -0300

    Aula 3: Relacionamentos com coleções(Java parte 8: Dominando as Collections)

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,27 @@
+package br.com.alura;
+
+import java.util.List;
+
+public class TestaCurso {
+
+    public static void main(String[] args) {
+
+        Curso javaColecoes = new Curso("Dominando as coleções do Java",
+                                       "Paulo Silveira");
+
+        List<Aula> aulas = javaColecoes.getAulas();
+        System.out.println(aulas);
+
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+        javaColecoes.adiciona(new Aula("Modelando com colecoes", 24));
+        System.out.println(aulas);
+
+
+
+
+
+
+
+    }
+}
