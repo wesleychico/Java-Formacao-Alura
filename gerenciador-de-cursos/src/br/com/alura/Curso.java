79cc6daa4ad96c8f5e74dd8b66a76d70dfcd1e24
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-04-16 14:21:00 -0300

    Aula 4: Mais práticas com relacionamentos (Java parte 8: Dominando as Collections)

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -8,7 +8,7 @@ public class Curso {
 
     private String nome;
     private String instrutor;
-    private List<Aula> aulas = new LinkedList<>();
+    private List<Aula>  aulas = new LinkedList<>();
 
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -29,5 +29,27 @@ public class Curso {
 
     public void adiciona(Aula aula){
         this.aulas.add(aula);
+    }
+
+    // evolucao java 8
+    public int getTempo(){
+        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+    }
+
+    // ******** For tradicional **********
+//    public int getTempo() {
+//        int tempoTotal = 0;
+//
+//        for (Aula aula : aulas) {
+//            tempoTotal += aula.getTempo();
+//        }
+//        return tempoTotal;
+//    }
+
+    @Override
+    public String toString() {
+        return "[Curso: " + this.getNome() +
+                ", tempo total: " + this.getTempo() +
+                ", aulas: [" + this.getAulas() + "] ]";
     }
 }
--- gerenciador-de-cursos/src/br/com/alura/TesteCurso2.java
+++ gerenciador-de-cursos/src/br/com/alura/TesteCurso2.java
@@ -1,4 +1,31 @@
 package br.com.alura;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TesteCurso2 {
+
+    public static void main(String[] args) {
+
+        Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+
+        List<Aula> TesteAulas = javaColecoes.getAulas();
+        System.out.println(TesteAulas);
+
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+        javaColecoes.adiciona(new Aula("Modelando com colecoes", 24));
+
+        System.out.println(TesteAulas);
+
+        List<Aula> aulasImutaveis = javaColecoes.getAulas();
+
+        List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+        Collections.sort(aulas);
+        System.out.println(aulas);
+
+        System.out.println(javaColecoes);
+
+    }
 }
