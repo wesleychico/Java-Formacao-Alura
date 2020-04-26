ce1d6c89a1950e9482332f919c22efd639d38946
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-04-26 09:58:27 -0300

    7.1 Removendo Empresa (Servlet part1)

--- java-servlet/gerenciador/src/br/com/caelum/gerenciador/servlet/Banco.java
+++ java-servlet/gerenciador/src/br/com/caelum/gerenciador/servlet/Banco.java
@@ -1,27 +1,45 @@
 package br.com.caelum.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 		Banco.lista.add(empresa);
 		Banco.lista.add(empresa2);				
 	}
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
 	
 	public static List<Empresa> getEmpresas() {
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if (emp.getId() == id) {
+				it.remove();
+			}			
+		}		
 	}
 	
 
