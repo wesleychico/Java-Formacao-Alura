4e6a3f42bd848ed9b932d75a183d3302855165a4
Wesley Chico <wesleyrocha.ti@gmail.com>
2020-04-30 08:12:31 -0300

    5.05 - Ordem de execucao no WEB.XMl

--- java-servlet/gerenciador/src/br/com/caelum/gerenciador/servlet/MonitoramentoFilter.java
+++ java-servlet/gerenciador/src/br/com/caelum/gerenciador/servlet/MonitoramentoFilter.java
@@ -9,7 +9,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter(urlPatterns = "/entrada")
+//@WebFilter(urlPatterns = "/entrada")
 public class MonitoramentoFilter implements Filter{
 
 	@Override
